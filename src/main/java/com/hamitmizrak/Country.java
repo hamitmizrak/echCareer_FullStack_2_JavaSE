3bd28e7a866ab36bd1b285dace66dd92d9a23239
Hamit Mızrak <hamitmizrak@gmail.com>
2023-08-14 15:12:28 +0300

    File, Innerclass

---
+++ src/main/java/com/hamitmizrak/Country.java
@@ -0,0 +1,42 @@
+package com.hamitmizrak;
+
+import java.util.Arrays;
+
+// Inner POJO
+public class Country {
+    // Field
+    private String countryName;
+
+    public String getCountryName() {
+        return countryName;
+    }
+
+    public void setCountryName(String countryName) {
+        this.countryName = countryName;
+    }
+
+    // Inner Class
+    public static class City {
+        private String cityName;
+
+        public String getCityName() {
+            return cityName;
+        }
+
+        public void setCityName(String cityName) {
+            this.cityName = cityName;
+        }
+    }
+
+    public static void main(String[] args) {
+        Country country = new Country();
+        country.setCountryName("Türkiye");
+
+        City city = new City();
+        city.setCityName("Sivas");
+
+        System.out.println(country.getCountryName() + " " + city.getCityName());
+    }
+}
+
+
