086ea1a070bb0be8fbe33b086da8167f6ff5ae9e
Hamit Mızrak <hamitmizrak@gmail.com>
2023-08-17 12:49:46 +0300

    Generics

---
+++ src/main/java/com/collections/GenericsData.java
@@ -0,0 +1,25 @@
+package com.collections;
+
+
+public class GenericsData<T> {
+
+    private T adi;
+
+    public T getAdi() {
+        return adi;
+    }
+
+    public void setAdi(T adi) {
+        this.adi = adi;
+    }
+
+    public GenericsData(T adi) {
+        this.adi = adi;
+    }
+
+    public static void main(String[] args) {
+        GenericsData genericsData=new GenericsData();
+        genericsData.setAdi(true);
+        System.out.println(genericsData.getAdi());
+    }
+}
