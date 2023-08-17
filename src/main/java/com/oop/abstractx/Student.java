6fa1df49f08e23ddd29820378791ab6582b54645
Hamit Mızrak <hamitmizrak@gmail.com>
2023-08-17 12:14:50 +0300

    Abstract

---
+++ src/main/java/com/oop/abstractx/Student.java
@@ -0,0 +1,11 @@
+package com.oop.abstractx;
+
+// POJO
+// POJO+constructor: BEAN
+public class Student extends Person {
+
+    @Override
+    public void govdesizMethod() {
+        System.out.println("Student Gövdesiz ");
+    }
+}
---
+++ src/main/java/com/oop/abstractx/Teacher.java
@@ -0,0 +1,10 @@
+package com.oop.abstractx;
+
+public class Teacher extends Person {
+
+    @Override
+    public void govdesizMethod() {
+        System.out.println("Teacher Gövdesiz ");
+    }
+
+}
